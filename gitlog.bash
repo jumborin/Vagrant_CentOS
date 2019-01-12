46dbd1b47a30f99b4026a718caa36113ce1ada09
jumborin <y.f.architect@gmail.com>
2019-01-12 20:48:59 +0900

    GitのログファイルをCSV形式で出力するシェルスクリプトを追加

---
+++ gitlog.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Gitのログをテキストファイルに出力する。
+git log > GitLog.log
+
+# 変数を設定
+COMMIT_MESSAGE="commit "
+COMMIT_TEMP_FILE=commit.tmp
+AUTHOR_MESSAGE="Author: "
+AUTHOR_TEMP_FILE=Author.tmp
+DATE_MESSAGE="Date:   "
+DATE_TEMP_FILE=Dates.tmp
+COMMENT_MESSAGE="    "
+COMMENT_TEMP_FILE=Comment.tmp
+MERGE_TEMP_FILE=merge.tmp
+RESULT_FILE=result.csv
+
+# ログを分析
+cat GitLog.log | grep "$COMMIT_MESSAGE" > $COMMIT_TEMP_FILE
+cat GitLog.log | grep "$AUTHOR_MESSAGE" > $AUTHOR_TEMP_FILE
+cat GitLog.log | grep "$DATE_MESSAGE" > $DATE_TEMP_FILE
+cat GitLog.log | grep "$COMMENT_MESSAGE" > $COMMENT_TEMP_FILE
+paste -d"," $COMMIT_TEMP_FILE $AUTHOR_TEMP_FILE >$MERGE_TEMP_FILE
+paste -d"," $MERGE_TEMP_FILE $DATE_TEMP_FILE > $MERGE_TEMP_FILE
+paste -d"," $MERGE_TEMP_FILE $COMMENT_TEMP_FILE > $RESULT_FILE
+
+
+# 一時ファイルを削除
+rm $COMMIT_TEMP_FILE
+rm $AUTHOR_TEMP_FILE
+rm $DATE_TEMP_FILE
+rm $COMMENT_TEMP_FILE
+rm $MERGE_TEMP_FILE
+
